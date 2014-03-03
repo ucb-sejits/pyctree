4fe5a33fcb919f4ba7ea36ef99fdb9351b217f2f
David Dinh <david@dinh.io>
2014-03-02 21:09:19 -0800

    Insert import and changed compile to ast.parse

--- pyctree/parse.py
+++ pyctree/parse.py
@@ -4,9 +4,11 @@ parse.py
 Parser to transform a python string into a raw python AST
 '''
 
+import ast
+
 """
 parse :: String -> AST object
 parses a string containing Python source and returns a raw Python AST
 """
 def parse(pysource):
-   return compile(pysource, "input to pyctree", "exec") 
+   return ast.parse(pysource, "input to pyctree", "exec") 
