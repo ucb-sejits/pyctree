0bef45003380dc31db6128c30aed00fd82df3807
David Dinh <david@dinh.io>
2014-03-02 21:23:10 -0800

    Fixed bugs

--- pyctree/__init__.py
+++ pyctree/__init__.py
@@ -1,5 +1,9 @@
+import sys
+import time
+
 import analysis
 import parse
+import ctree
 
 """
 compile_to_ctree :: String -> [configuration object] -> Ctree object
@@ -8,10 +12,13 @@ ctree object.
 """
 def compile_to_ctree(pysource):
     sys.setrecursionlimit(100000)
+    t0 = time.time()
+
     print 'Generating ctree...'
     raw_ast = parse(pysource)
-    annotated_ast = analyze(raw_ast)
-    c_ast = transform_to_ctree(annotated_ast)
+    annotated_ast = analysis.analyze(raw_ast)
+    c_ast = ctree.transform_to_ctree(annotated_ast)
+    
     print 'Finished generating ctree'
     print '[elapsed time: %.2f seconds]' % (time.time() - t0)
     return c_ast
